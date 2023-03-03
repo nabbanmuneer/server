59eeaf2b3534a869870f960cdabe5ab26cf4b2ec
Nabban muneer pp <nabbanmuneerpp@gmail.com>
2023-03-03 18:14:51 +0530

    cors changes

--- config/allowedOrgin.js
+++ config/allowedOrgin.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
     "http://localhost:5173",
+    "https://letshire.onrender.com",
   ];
   
   module.exports = allowedOrigins;
